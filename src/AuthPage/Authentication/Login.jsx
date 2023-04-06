00a003ec611a502c42d8965261a5240eaa362b60
MdMoin788 <developermdmoin@gmail.com>
2023-04-06 16:01:46 +0530

    Login

--- src/AuthPage/Authentication/Login.jsx
+++ src/AuthPage/Authentication/Login.jsx
@@ -4,21 +4,17 @@ const Login = () => {
 
 
   return (
-    <div className='container' style={{width:"40%", margin:"auto", marginTop:"10%",}}>
+    <div className='container card p-4' style={{ width: "25%", margin: "auto", marginTop: "10%", }}>
       <div className="row">
-
-
-
-
         {/* <!-- Pills navs --> */}
-        <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-          <li className="nav-item" onClick={() => setShow(true)}>
-            <a className={`nav-link ${show == true && "active"}`}
-            >Login</a>
+        <ul className="nav nav-pills nav-justified mb-3"  >
+          <li className="nav-item ms-2" onClick={() => setShow(true)}>
+            <span className={`nav-link  ${show == true && "active"}`}
+            >Login</span>
           </li>
           <li className="nav-item" onClick={() => setShow(false)}>
-            <a className={`nav-link ${show == false && "active"}`}
-            >Register</a>
+            <span className={`nav-link  ${show == false && "active"}`}
+            >Register</span>
           </li>
         </ul>
         {/* <!-- Pills navs --> */}
@@ -31,7 +27,7 @@ const Login = () => {
             <div className="">
               <form>
                 <div className="text-center mb-3">
-                  <p>Sign in with:</p>
+                  <p>Sign in with</p>
                   <button type="button" className="btn btn-link btn-floating mx-1">
                     <i className="fab fa-facebook-f"></i>
                   </button>
@@ -49,38 +45,35 @@ const Login = () => {
                   </button>
                 </div>
 
-                <p className="text-center">or:</p>
+                <p className="text-center">or</p>
 
                 {/* <!-- Email input --> */}
-                <div className="form-outline mb-4">
-                  <input type="email" id="loginName" className="form-control" />
-                  <label className="form-label" for="loginName">Email or username</label>
+                <div className="form-outline mb-2">
+                  <label className="form-label" for="loginName">Email or username</label> 
+                  <input type="email" className="form-control" />
                 </div>
 
                 {/* <!-- Password input --> */}
-                <div className="form-outline mb-4">
-                  <input type="password" id="loginPassword" className="form-control" />
+                <div className="form-outline mb-2">
                   <label className="form-label" for="loginPassword">Password</label>
+                  <input type="password" className="form-control" />
                 </div>
 
                 {/* <!-- 2 column grid layout --> */}
-                <div className="row mb-4">
-                  <div className="col-md-6 d-flex justify-content-center">
-                    {/* <!-- Checkbox --> */}
-                    <div className="form-check mb-3 mb-md-0">
-                      <input className="form-check-input" type="checkbox" value="" id="loginCheck" checked />
-                      <label className="form-check-label" for="loginCheck"> Remember me </label>
-                    </div>
-                  </div>
+                <div className="row mb-3">
 
-                  <div className="col-md-6 d-flex justify-content-center">
-                    {/* <!-- Simple link --> */}
-                    <a href="#!">Forgot password?</a>
+
+                  {/* <div className="col-md-6 d-flex justify-content-center">
+                    <a >Forgot password?</a>
+                  </div> */}
+                  <div className="text-center">
+                    <a >Forgot password?</a>
+
                   </div>
                 </div>
 
                 {/* <!-- Submit button --> */}
-                <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
+                <button className="btn btn-primary btn-block mb-4 col-12">Sign in</button>
 
                 {/* <!-- Register buttons --> */}
                 <div className="text-center">
@@ -96,7 +89,7 @@ const Login = () => {
             <div className="">
               <form>
                 <div className="text-center mb-3">
-                  <p>Sign Up with:</p>
+                  <p>Sign Up with</p>
                   <button type="button" className="btn btn-link btn-floating mx-1">
                     <i className="fab fa-facebook-f"></i>
                   </button>
@@ -114,24 +107,24 @@ const Login = () => {
                   </button>
                 </div>
 
-                <p className="text-center">or:</p>
+                <p className="text-center">or</p>
 
                 {/* <!-- Email input --> */}
-                <div className="form-outline mb-4">
+                <div className="form-outline mb-2">
+                  <label className="form-label" for="loginName">Email or username</label> 
                   <input type="email" id="loginName" className="form-control" />
-                  <label className="form-label" for="loginName">Email or username</label>
                 </div>
 
                 {/* <!-- Password input --> */}
-                <div className="form-outline mb-4">
-                  <input type="password" id="loginPassword" className="form-control" />
+                <div className="form-outline mb-2">
                   <label className="form-label" for="loginPassword">Password</label>
+                  <input type="password" id="loginPassword" className="form-control" />
                 </div>
 
 
 
                 {/* <!-- Submit button --> */}
-                <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
+                <button type="submit" className="btn btn-primary btn-block mb-3 col-12 mt-4">Sign Up</button>
 
                 {/* <!-- Register buttons --> */}
 
