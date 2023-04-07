9ec5d8d74ae0ac9fa8fc27c66b4a70bd6297942e
MdMoin788 <developermdmoin@gmail.com>
2023-04-07 13:54:15 +0530

    responsive

--- src/Components/Home/Navigation.jsx
+++ src/Components/Home/Navigation.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
-
+import "./Navigation.css"
 const Navigation = () => {
     return (
         <>
-            <img className='ins-logo px-0 mb-4' style={{ width: "100%" }} src="https://github.com/MdMoin788/ALL_IMAGES_PREFESSIONALLY/blob/main/AssertsInsta/Instagramlogo.png?raw=true" alt="logo" />
-            <div className='ms-4 ' style={{ width: "100%", display: "flex", flexDirection: 'column', justifyContent: "", height: "100%" }}>
+            <img className='ins-logo px-0 mb-5' style={{ width: "100%" }} src="https://github.com/MdMoin788/ALL_IMAGES_PREFESSIONALLY/blob/main/AssertsInsta/Instagramlogo.png?raw=true" alt="logo" />
+            <div className='ms-4  sidebanavigation '>
                 <div className='d-flex mb-3'>
-                    <i class="fa-solid fa-house fs-4"></i>
-                    <p className='ms-2 '>Home</p>
-                </div>
-                <div className='d-flex mb-3'>
-                    <i class="fa-solid fa-magnifying-glass fs-4"></i>
-                    <p className='ms-2 fw-bold'>Home</p>
+                    <i class="fa-solid fa-house fs-5"></i>
+                    <p className='ms-2 show '>Home</p>
                 </div>
                 <div className='d-flex mb-3'>
-                    <i class="fa-sharp fa-regular fa-compass fs-4"></i>
-                    <p className='ms-2 fw-bold'>Home</p>
+                    <i class="fa-solid fa-magnifying-glass fs-5  mt-1"></i>
+                    <p className='ms-2 show '>Search</p>
+                </div>
+                <div className='d-flex mb-3 '>
+                    <i class="fa-sharp fa-regular fa-compass fs-5 mt-1 show"></i>
+                    <p className='ms-2 show '>Explore</p>
+                </div>
+                <div className='d-flex mb-3 '>
+                    <i class="fa-solid fa-video fs-5 mt-1"></i>
+                    <p className='ms-2 show '>Reeals</p>
+                </div>
+                <div className='d-flex mb-3 '>
+                    <i class="fa-regular fa-message fs-5 mt-1 show"></i>
+                    <p className='ms-2 show '>Messages</p>
+                </div>
+                <div className='d-flex mb-3 '>
+                    <i class="fa-regular fa-heart fs-5 mt-1 show"></i>
+                    <p className='ms-2 show '>Notification</p>
                 </div>
                 <div className='d-flex mb-3'>
-                    <i class="fa-solid fa-video fs-4"></i>
-                    <p className='ms-2 fw-bold'>Home</p>
+                    <i class="fa-sharp fa-solid fa-plus mt-1 fs-5"></i>
+                    <p className='ms-2 show '>Create</p>
                 </div>
-                <div className='d-flex mb-3'>
-                    <i class="fa-regular fa-heart fs-4"></i>
-                    <p className='ms-2 fw-bold'>Home</p>
+                <div className='d-flex mb-3 '>
+                    <i class="fa-sharp fa-regular fa-circle mt-1 fs-2"></i>
+                    <p className='ms-2 show '>Profile</p>
                 </div>
-                <div className='d-flex mb-3 mt-4'>
-                <i className="fa-solid fa-bars fs-4"></i>
-                    <p className='ms-2 fw-bold'>More</p>
+                <div className='d-flex mb-3 mt-4 '>
+                    <i className="fa-solid fa-bars fs-5 mt-1 show"></i>
+                    <p className='ms-2 show '>More</p>
                 </div>
-               
+
             </div>
 
         </>
